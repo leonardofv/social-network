525956f69b4e26964a72436b0afe07b6925bcf8e
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-07-14 10:51:48 -0300

    Added profile and search sections.

---
+++ client/app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Essa é a Página de Pesquisa</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  title: { color: '#fff' }
+});
