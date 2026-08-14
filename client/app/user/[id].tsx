53e32f4ff6dd37772dae4b7662a503c90d4da4c8
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-08-14 18:52:37 -0300

    Correction of the photo's position in the feed

--- client/app/user/[id].tsx
+++ client/app/user/[id].tsx
@@ -83,7 +83,7 @@ export default function UserDetailScreen() {
       data={posts}
       keyExtractor={(item) => item.id.toString()}
       numColumns={GRID_COLUMNS}
-      columnWrapperStyle={styles.gridRow}
+      columnWrapperStyle={[styles.gridRow, { width: contentWidth}]}
       ItemSeparatorComponent={() => <View style={styles.gridSeparator} />}
       renderItem={({ item }) => (
         <Pressable onPress={() => router.push(`/post/${item.id}`)}>
